9fc935c7df705322146cd7bd3f4a9d44a8bfc167
Oz Parl <ozzy7o@icloud.com>
2019-04-18 20:26:20 -0400

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+var userName = prompt("what is your name?");
+var userLastName = prompt("what is your name?");
+var userAge = prompt("what is your Age?");
+
+alert("Its nice to meet you " + userName + " " + userLastName + "!");
+
+alert(" I cannot believe your only " + userAge +(".") + " You look wayyyy younger! ");
+
+console.log("Also Great to meet you, " + userName + " I cannot believe your only " + userAge +"." +" You look wayyyy younger! ");
